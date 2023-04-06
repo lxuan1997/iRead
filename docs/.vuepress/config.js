d2896f1f21683115da766e2bec59ed54d0875f69
luoxuan <lxuan2497@gmail.com>
2023-04-06 17:21:58 +0800

    📚 无伤跑法

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
+    title: 'iRead',
+    description: '这是我的 VuePress 站点',
     base: '/iRead'
 })
