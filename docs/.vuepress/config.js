c4029da041152d110443f8a66db2ee33daf176fe
luoxuan <lxuan2497@gmail.com>
2023-04-10 23:35:04 +0800

    配置调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,15 +4,15 @@ import { navbar } from "./navbar";
 import { sidebar } from "./sidebar";
 
 export default defineUserConfig({
+    base: '/iRead/',
     lang: 'zh-CN',
     title: '',
     head: [
-        ['link', { rel: 'icon', href: '/images/logo-icon.png' }],
+        ['link', { rel: 'icon', href: '/iRead/images/logo-icon.png' }],
         // 添加apple icon
-        ['link', { rel: 'apple-touch-icon', href: '/images/logo-icon.png' }]
+        ['link', { rel: 'apple-touch-icon', href: '/iRead/images/logo-icon.png' }]
     ],
     description: 'keep reading , day day up',
-    base: '/iRead',
     theme: defaultTheme({
         logo: '/images/logo.png',
         logoDark: '/images/logo-dark.png',
